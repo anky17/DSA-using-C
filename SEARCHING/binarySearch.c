b7838d7bba94e213ab5a64d9665ea1b10811df19
Ankit <ankitdiyal17@gmail.com>
2024-05-09 20:35:50 +0545

    binary search code enhanced

--- SEARCHING/binarySearch.c
+++ SEARCHING/binarySearch.c
@@ -3,19 +3,21 @@
 #include <stdio.h>
 
 // Function to perform binary search
-int binarySearch(int arr[], int l, int r, int x) {
-  while (l <= r) {
-    int m = l + (r - l) / 2;
+int binarySearch(int arr[], int size, int x) {
+  int left = 0;
+  int right = size - 1;
+  while (left <= right) {
+    int middle = (left) + (right) / 2;
 
     // Check if x is present at mid
-    if (arr[m] == x) return m;
+    if (arr[middle] == x) return middle;
 
     // If x greater than mid then we ignore left half
-    if (arr[m] < x) l = m + 1;
+    if (arr[middle] < x) (left) = middle + 1;
 
     // If x is smaller than mid then we ignore right half
     else
-      r = m - 1;
+      right = middle - 1;
   }
 
   // If we reach here then the element was not present
@@ -26,8 +28,8 @@ int main() {
   int arr[] = {2, 3, 6, 10, 12, 30};
   // int arr[] = {9, 8, 7, 6};
   int n = sizeof(arr) / sizeof(arr[0]);
-  int x = 30;
-  int result = binarySearch(arr, 0, n - 1, x);
+  int x = 2;
+  int result = binarySearch(arr, n, x);
   if (result == -1)
     printf("Element is not present in array");
   else
